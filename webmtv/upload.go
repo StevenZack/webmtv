8064bb0d7fdc72fb1d7f69b797ad6be6436d7f76
StevenZack <zigzigcheers@gmail.com>
2017-07-26 15:08:29 +0800

    finish playlist

--- webmtv/upload.go
+++ webmtv/upload.go
@@ -117,6 +117,12 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 	//return upload-succeed page
 	ReturnInfo(w, "succeed", "/")
 }
+
+type EditPageData struct {
+	Me        Video
+	PlayLists []PlayList
+}
+
 func EditVideo(w http.ResponseWriter, r *http.Request) {
 	vid := r.FormValue("vid")
 	sid, err := r.Cookie("WEBMTV-SESSION-ID")
@@ -131,7 +137,7 @@ func EditVideo(w http.ResponseWriter, r *http.Request) {
 		ReturnInfo(w, "please log in first", "/login")
 		return
 	}
-	fundVideo := Video{}
+	epd := EditPageData{}
 	s, err := mgo.Dial("127.0.0.1")
 	if err != nil {
 		go RestartMongodb()
@@ -140,33 +146,86 @@ func EditVideo(w http.ResponseWriter, r *http.Request) {
 	}
 	defer s.Close()
 	cv := s.DB("webmtv").C("videos")
-	err = cv.Find(bson.M{"vid": vid}).One(&fundVideo)
+	err = cv.Find(bson.M{"vid": vid}).One(&epd.Me)
 	if err != nil {
 		ReturnInfo(w, "No such video", "")
 		return
 	}
-	if fundVideo.OwnerID != u.ID {
+	if epd.Me.OwnerID != u.ID {
 		ReturnInfo(w, "You don't have permission to edit this video", "")
 		return
 	}
-	if r.Method == "GET" { // GET
-		t, _ := template.ParseFiles("./html/editvideo.html")
-		t.Execute(w, fundVideo)
+	cpl := s.DB("webmtv").C("playlists")
+	err = cpl.Find(bson.M{"ownerid": u.ID}).All(&epd.PlayLists)
+	if err != nil {
+		ReturnInfo(w, err.Error(), "")
 		return
 	}
+
+	if r.Method == "GET" { // GET
+		t, _ := template.ParseFiles("./html/editvideo.html")
+		t.Execute(w, epd)
+		return
+	}
+
+	rPlayList := r.FormValue("playlist")
+	if rPlayList == "" {
+		ReturnInfo(w, "playList invalid", "")
+		return
+	}
+	plID := ""
+	existedPlayListFlag := false
+	for _, v := range epd.PlayLists {
+		if v.Title == rPlayList {
+			plID = v.Vid
+			existedPlayListFlag = true
+			break
+		}
+	}
+	if !existedPlayListFlag {
+		plID = NewToken()
+		err = cpl.Insert(&PlayList{
+			Title:      rPlayList,
+			Vid:        plID,
+			Cover:      epd.Me.Cover,
+			OwnerID:    u.ID,
+			ListLength: 1,
+		})
+		if err != nil {
+			ReturnInfo(w, err.Error(), "")
+			return
+		}
+	}
+
 	//POST
 	err = cv.Update(bson.M{"vid": vid}, bson.M{"$set": bson.M{
-		"title":        r.FormValue("title"),
-		"vurl":         r.FormValue("video"),
-		"cover":        r.FormValue("cover"),
-		"iswebtorrent": r.FormValue("videoType") == "webtorrent",
+		"title":         r.FormValue("title"),
+		"vurl":          r.FormValue("video"),
+		"cover":         r.FormValue("cover"),
+		"iswebtorrent":  r.FormValue("videoType") == "webtorrent",
+		"playlisttitle": rPlayList,
+		"playlistid":    plID,
 	}})
 	if err != nil {
 		ReturnInfo(w, "Update video info failed:"+err.Error(), "")
 		return
 	}
+	//update playlist length
+	oldLength, _ := cv.Find(bson.M{"playlistid": epd.Me.PlayListID}).Count()
+	err = cpl.Update(bson.M{"vid": epd.Me.PlayListID}, bson.M{"$set": bson.M{"listlength": oldLength}})
+	if err != nil {
+		ReturnInfo(w, err.Error(), "")
+		return
+	}
+	newLength, _ := cv.Find(bson.M{"playlistid": plID}).Count()
+	err = cpl.Update(bson.M{"vid": plID}, bson.M{"$set": bson.M{"listlength": newLength}})
+	if err != nil {
+		ReturnInfo(w, err.Error(), "")
+		return
+	}
 	ReturnInfo(w, "Succeed", "/u?id="+u.ID)
 }
+
 func DeleteVideo(w http.ResponseWriter, r *http.Request) {
 	vid := r.FormValue("vid")
 	sid, err := r.Cookie("WEBMTV-SESSION-ID")
@@ -202,6 +261,13 @@ func DeleteVideo(w http.ResponseWriter, r *http.Request) {
 	err = cv.Remove(bson.M{"vid": vid})
 	if err != nil {
 		ReturnInfo(w, "delete failed:"+err.Error(), "")
+		return
+	}
+	newLength, _ := cv.Find(bson.M{"playlistid": fundVideo.PlayListID}).Count()
+	cpl := s.DB("webmtv").C("playlists")
+	err = cpl.Update(bson.M{"vid": fundVideo.PlayListID}, bson.M{"$set": bson.M{"listlength": newLength}})
+	if err != nil {
+		ReturnInfo(w, err.Error(), "")
 		return
 	}
 	ReturnInfo(w, "Succeed", "/u?id="+u.ID)
